def8055afd9dfc137cf8c5a7181440f4519e4b7c
syuheifujita <mcslr772ed@gmail.com>
2019-10-04 09:28:55 +0900

    #1 ボタンの処理に，ランダムに数字を表示させる処理を実装

--- app/src/main/java/com/example/android_omikuji/MainActivity.kt
+++ app/src/main/java/com/example/android_omikuji/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,7 +13,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         omikuji_button.setOnClickListener() {
-            Log.v("MainActivity","ButtonClicked")
+//            Log.v("MainActivity","ButtonClicked")
+
+            val n = Random.nextInt(3) //乱数代入
+            result_text.text = n.toString()
         }
     }
 }
